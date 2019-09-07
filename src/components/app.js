a5a8aea23cfbef7f3049969248fff7bbbad75ad0
Ryan Bednar <ryanbednar@Ryans-MBP.fios-router.home>
2019-09-07 18:17:55 -0400

    reorganize logic in App.js
    - also create index.css for storing SCSS imports from components

--- src/components/app.js
+++ src/components/app.js
@@ -1,23 +1,9 @@
 import React from "react";
-import "./app.scss";
-import imgFile from "../../assests/image/Favicon.png";
 
 const App = () => {
   return (
-    <div className="full-screen">
-      <div>
-        <h1>
-          asdfasdf React Page <img src={imgFile} width="30" height="30" />{" "}
-        </h1>
-        <br />
-        <a
-          className="button-line"
-          href="https://github.com/deityhub"
-          target="_blank"
-        >
-          Know more now
-        </a>
-      </div>
+    <div className="app">
+      Places Component
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app";
+
+import App from "./components/App";
+import "./index.scss";
 
 ReactDOM.render(<App />, document.getElementById("app"));
