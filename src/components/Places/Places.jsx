cbe2cc119c17dfece8f142b0caadae9eb443a15b
Ryan Bednar <ryanbednar@Ryans-MBP.fios-router.home>
2019-09-07 20:19:50 -0400

    add mock data, create getter to render a list of places

--- src/components/Places/Places.jsx
+++ src/components/Places/Places.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
+import data from './data';
+
 export class Places extends Component {
+  get places() {
+    return data.map((place, index) => {
+      const { name, stars, reviews, address } = place;
+
+      return (
+        <div className="places-place" key={index}>
+          <div>{name}</div>
+          <div>{stars}</div>
+          <div>{reviews}</div>
+          <div>{address}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
-      <div>Places Component</div>
-    )
+      <div className="places">{this.places}</div>
+    );
   }
 }
 
