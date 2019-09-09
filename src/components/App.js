05808b26277080dd215ca7292978e42ac5f99f76
Ryan Bednar <ryanbednar@Ryans-MBP.fios-router.home>
2019-09-08 20:50:57 -0400

    add sort buttons to FilterBar and on click methods
    also:
    - converted App to a class level component
    - added constructors to App and FilterBar
    - create and implement ErrorBoundary component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 import FilterBar from './FilterBar/FilterBar';
 import PlacesList from './PlacesList/PlacesList';
 
 import data from '../api/data';
 
-const App = () => {
-  return (
-    <div className="app">
-      <FilterBar />
-      <PlacesList data={data} />
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(filters) {
+    console.log('');
+    console.log('App - handleSubmit');
+    console.log('  filters:', filters);
+  }
+
+  render() {
+    return (
+      <ErrorBoundary>
+        <div className="app">
+          <FilterBar handleSubmit={this.handleSubmit} />
+          <PlacesList data={data} />
+        </div>
+      </ErrorBoundary>
+    );
+  }
 };
 
 export default App;
--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
 export class FilterBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSort = this.onSort.bind(this);
+
+    this.state = {
+      sortBy: 'Highest Rated'
+    };
+  }
+
+  onSort(sortBy) {
+    console.log('');
+    console.log('FilterBar - onSort');
+    console.log('  sortBy:', sortBy);
+
+    this.setState({
+      sortBy
+    });
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+    
     return (
-      <div>FilterBar Placeholder</div>
+      <div>
+        <button onClick={() => this.onSort('Highest Rated')}>Highest Rated</button>
+        <button onClick={() => this.onSort('Most Reviewed')}>Most Reviewed</button>
+        <button onClick={() => handleSubmit(this.state)}>Submit</button>
+      </div>
     );
   }
 }
