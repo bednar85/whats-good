90bd7ee21f74afe29db38301b8f96d37457e16ac
Ryan Bednar <ryanbednar@Ryans-MBP.fios-router.home>
2019-09-12 18:23:03 -0400

    simplify App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,18 @@
 import React, { Component } from 'react';
 
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 import FilterBar from './FilterBar/FilterBar';
 import PlacesList from './PlacesList/PlacesList';
 
-import data from '../api/data';
+// move this to PlacesList
+import initialData from '../api/data';
 
 export class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(filters) {
-    console.log('');
-    console.log('App - handleSubmit');
-    console.log('  filters:', filters);
-  }
-
   render() {
     return (
-      <ErrorBoundary>
-        <div className="app">
-          <FilterBar handleSubmit={this.handleSubmit} />
-          <PlacesList data={data} />
-        </div>
-      </ErrorBoundary>
+      <div className="app">
+        <FilterBar />
+        <PlacesList data={initialData} />
+      </div>
     );
   }
 };
