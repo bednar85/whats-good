9ac7a53a12a3cdd0a08228892e10af10805f3d84
Ryan Bednar <ryanbednar@Ryans-MBP.fios-router.home>
2019-09-14 11:17:51 -0400

    update application module, bind actions and update action calls in Counter.jsx

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+import { actions } from '../../modules/application';
+
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.actions = props.actions;
+  }
+
   render() {
     return (
       <div>
         <div>{this.props.count}</div>
         <div>
           <button
-            onClick={() => this.props.dispatch({ type: 'DECREMENT'})}>
+            onClick={this.actions.decrementCounter}>
               Decrement
           </button>
           <button
-            onClick={() => this.props.dispatch({ type: 'INCREMENT'})}>
+            onClick={this.actions.incrementCounter}>
               Increment
           </button>
         </div>
@@ -21,10 +30,15 @@ class Counter extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    count: state.count
-  }
-}
+const mapStateToProps = state => ({
+  count: state.count
+});
 
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(
+    Object.assign({}, actions),
+    dispatch
+  )
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/modules/application/index.js
+++ src/modules/application/index.js
@@ -1,15 +1,35 @@
+// Action Types
+const COUNTER_INCREMENT = 'application/COUNTER_INCREMENT';
+const COUNTER_DECREMENT = 'application/COUNTER_DECREMENT';
+
+// Actions
+const incrementCounter = () => ({
+  type: COUNTER_INCREMENT
+});
+
+const decrementCounter = () => ({
+  type: COUNTER_DECREMENT
+});
+
+export const actions = {
+  incrementCounter,
+  decrementCounter
+};
+
+// Initial State
 const initialState = {
   count: 0
-}
+};
 
+// Reducer
 const counterReducer = (state = initialState, action) => {
   switch(action.type) {
-    case 'INCREMENT':
+    case COUNTER_INCREMENT:
       return {
         ...state,
         count: state.count + 1
       };
-    case 'DECREMENT':
+    case COUNTER_DECREMENT:
       return {
         ...state,
         count: state.count - 1 < 0 ? 0 : state.count - 1
@@ -17,6 +37,6 @@ const counterReducer = (state = initialState, action) => {
     default:
       return state;
   }
-}
+};
 
 export default counterReducer;
