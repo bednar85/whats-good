b63e3841c2aacee682cb6f7d4ecc7a408540369b
Ryan Bednar <ryanbednar@Ryans-MBP.fios-router.home>
2019-09-14 17:00:05 -0400

    add additional Radio Groups
    also:
    - set default values for Radio Groups
    - set some Radios to be disabled
    - tweak styles

--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -8,7 +8,9 @@ export class FilterBar extends Component {
     this.handleChange = this.handleChange.bind(this);
 
     this.state = {
-      sortBy: 'Highest Rated'
+      sortBy: 'Highest Rated',
+      distance: 1.5, 
+      hours: "Show All"
     };
   }
 
@@ -21,7 +23,11 @@ export class FilterBar extends Component {
   render() {
     const { sortBy } = this.state;
 
-    const radioStyle = {
+    const radioGroupStyles = {
+      verticalAlign: 'top'
+    };
+
+    const radioStyles = {
       display: 'block',
       height: '30px',
       lineHeight: '30px',
@@ -31,34 +37,90 @@ export class FilterBar extends Component {
       <div>
         <Radio.Group
           name="sortBy"
+          style={radioGroupStyles}
           onChange={this.handleChange}
           value={this.state.sortBy}
         >
           <Radio
-            style={radioStyle}
+            style={radioStyles}
             disabled
             value="Distance From You"
           >
             Distance From You
           </Radio>
           <Radio
-            style={radioStyle}
+            style={radioStyles}
             disabled
             value="Distance From Selected Location"
           >
             Distance From Selected Location
           </Radio>
           <Radio
-            style={radioStyle}
+            style={radioStyles}
             value="Highest Rated"
           >
             Highest Rated
           </Radio>
           <Radio
-            style={radioStyle}
+            style={radioStyles}
             value="Most Reviewed"
           >
             Most Reviewed
+          </Radio>
+        </Radio.Group>
+        <Radio.Group
+          name="distance"
+          style={radioGroupStyles}
+          onChange={this.handleChange}
+          value={this.state.distance}
+        >
+          <Radio
+            style={radioStyles}
+            disabled
+            value={0.5}
+          >
+            Within 4 Blocks
+          </Radio>
+          <Radio
+            style={radioStyles}
+            disabled
+            value={1.5}
+          >
+            Walking (1.5 mi)
+          </Radio>
+          <Radio
+            style={radioStyles}
+            disabled
+            value={5}
+          >
+            Bus/Subway (5 mi)
+          </Radio>
+          <Radio
+            style={radioStyles}
+            disabled
+            value={Infinity}
+          >
+            Show All
+          </Radio>
+        </Radio.Group>
+        <Radio.Group
+          name="hours"
+          style={radioGroupStyles}
+          onChange={this.handleChange}
+          value={this.state.hours}
+        >
+          <Radio
+            style={radioStyles}
+            disabled
+            value="Open Now"
+          >
+            Open Now
+          </Radio>
+          <Radio
+            style={radioStyles}
+            value="Show All"
+          >
+            Show All
           </Radio>
         </Radio.Group>
       </div>
