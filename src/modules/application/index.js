3c5679217d332e9d72e46024839e95f067248911
Ryan Bednar <ryanbednar@Ryans-MBP.fios-router.home>
2019-09-14 17:37:12 -0400

    create a new action for updating the filters in the store

--- src/modules/application/index.js
+++ src/modules/application/index.js
@@ -1,6 +1,7 @@
 // Action Types
 const COUNTER_INCREMENT = 'application/COUNTER_INCREMENT';
 const COUNTER_DECREMENT = 'application/COUNTER_DECREMENT';
+const FILTERS_UPDATE = 'application/FILTERS_UPDATE';
 
 // Actions
 const incrementCounter = () => ({
@@ -11,19 +12,38 @@ const decrementCounter = () => ({
   type: COUNTER_DECREMENT
 });
 
+const updateFilters = payload => ({
+  type: FILTERS_UPDATE,
+  payload
+});
+
 export const actions = {
   incrementCounter,
-  decrementCounter
+  decrementCounter,
+  updateFilters
 };
 
 // Initial State
 const initialState = {
-  count: 0
+  count: 0,
+  filters: {
+    sortBy: 'Highest Rated',
+    distance: 1.5, 
+    hours: 'Show All'
+  }
 };
 
 // Reducer
 const counterReducer = (state = initialState, action) => {
   switch(action.type) {
+    case FILTERS_UPDATE:
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          ...action.payload
+        }
+      };
     case COUNTER_INCREMENT:
       return {
         ...state,
