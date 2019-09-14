a04297b70e4cb4d9c4802ab50dbcbb88eb3c89be
Ryan Bednar <ryanbednar@Ryans-MBP.fios-router.home>
2019-09-14 17:36:55 -0400

    enable Redux DevTools Extension when creating the store

--- src/modules/store.js
+++ src/modules/store.js
@@ -2,6 +2,9 @@ import { createStore } from 'redux';
 
 import counterReducer from './application';
 
-const store = createStore(counterReducer);
+const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
