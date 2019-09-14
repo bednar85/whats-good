2fabb651cf63fb8f5081e1b9297c1e2a15dec97a
Ryan Bednar <ryanbednar@Ryans-MBP.fios-router.home>
2019-09-14 10:45:42 -0400

    move simple store out of App.js and into modules folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
@@ -8,28 +7,7 @@ import Counter from './components/Counter/Counter';
 import FilterBar from './components/FilterBar/FilterBar';
 import PlacesList from './components/PlacesList/PlacesList';
 
-const initialState = {
-  count: 0
-}
-
-const counterReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case 'INCREMENT':
-      return {
-        ...state,
-        count: state.count + 1
-      };
-    case 'DECREMENT':
-      return {
-        ...state,
-        count: state.count - 1 < 0 ? 0 : state.count - 1
-      };
-    default:
-      return state;
-  }
-}
-
-const store = createStore(counterReducer);
+import store from './modules/store';
 
 console.log(store);
 
