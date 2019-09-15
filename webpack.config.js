9183fe93ca579d7df16121a9de93ba86ac3b252f
Ryan Bednar <ryanbednar@Ryans-MBP.fios-router.home>
2019-09-15 14:17:38 -0400

    setup ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        use: ['babel-loader', 'eslint-loader']
       },
       {
         test: /\.(css|scss)$/,
