99b1d563f983b82d9a89b8387d90fd974a338216
Ryan Bednar <ryanbednar@Ryans-MBP.fios-router.home>
2019-09-15 01:16:31 -0400

    add hours related logic to PlacesList component

--- src/components/PlacesList/PlacesList.jsx
+++ src/components/PlacesList/PlacesList.jsx
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
+
+import { Rate, Typography } from 'antd';
 
 import data from '../../api/data';
 
+const { Text, Title } = Typography;
+
 export class PlacesList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getHoursData = this.getHoursData.bind(this);
+  }
+
+  getHoursData(hours) {
+    const today = moment().format('ddd');
+    const tomorrow = moment().add(1, 'day').format('ddd');
+
+    const todaysHours = hours[today];
+    const tomorrowsHours = hours[tomorrow];
+
+    let isOpen = undefined;
+
+    if (todaysHours.display === 'Closed') {
+      isOpen = false;
+    } else {
+      const opens = todaysHours.opens.minute
+        ? moment().set('hour', todaysHours.opens.hour, 'minute', todaysHours.opens.minute)
+        : moment().set('hour', todaysHours.opens.hour)
+      
+      const closes = todaysHours.closes.minute
+        ? moment().set('hour', todaysHours.closes.hour, 'minute', todaysHours.closes.minute)
+        : moment().set('hour', todaysHours.closes.hour)
+      
+      isOpen = moment().isBetween(opens, closes, '[)');
+    }
+
+    return {
+      todaysHours: `${today} - ${todaysHours.display}`,
+      tomorrowsHours: `${tomorrow} - ${tomorrowsHours.display}`,
+      isOpen
+    };
+  }
+
   get sortedData() {
     const { filters } = this.props;
 
@@ -25,15 +66,22 @@ export class PlacesList extends Component {
 
   get places() {
     return this.sortedData.map((place, index) => {
-      const { name, stars, reviews, address } = place;
+      const { name, stars, reviews, address, hours } = place;
+      const { todaysHours, tomorrowsHours, isOpen } = this.getHoursData(hours);
+
+      const closedClass = !isOpen && 'place--is-closed';
 
       return (
-        <div className="place" key={index}>
+        <div className={`place ${closedClass}`} key={index}>
           <div className="place-content">
-            <h3 className="place-name">{name}</h3>
-            <p className="place-stars">{stars} Stars</p>
-            <p className="place-reviews">{reviews} Reviews</p>
-            <p className="place-address">{address}</p>
+            <Title className="place-name" level={4}>{name}</Title>
+            <Rate disabled allowHalf defaultValue={stars} />
+            <Text className="place-reviews">{reviews} Reviews</Text>
+            <Text className="place-address">{address}</Text>
+            <div className="place-hours">
+              <Text className="place-hours">{todaysHours}</Text>
+              <Text className="place-hours">{tomorrowsHours}</Text>
+            </div>
           </div>
         </div>
       );
