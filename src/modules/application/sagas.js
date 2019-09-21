9e821f87526553331a825b10afa5fb2b6a0d4b47
Ryan <bednar85@gmail.com>
2019-09-21 18:46:53 -0400

    update loadDataWorker so it splits off based on action key

--- src/modules/application/sagas.js
+++ src/modules/application/sagas.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { actions, types } from '.';
 import { metersToMiles } from '../../utils';
 
-function getYelpData(location) {
+function yelpBusinessSearch(location) {
   return axios.get(
     `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search`,
     {
@@ -20,12 +20,16 @@ function getYelpData(location) {
   );
 }
 
-function* loadDataWorker(action) {
+// function yelpBusinessDetails(ids) {
+//   console.log('businessIds:', businessIds);
+// }
+
+function* loadPlacesData(action) {
   const { latitude, longitude } = action.payload;
   const location = { latitude, longitude };
 
   try {
-    const response = yield call(getYelpData, location);
+    const response = yield call(yelpBusinessSearch, location);
 
     const processedData = response.data.businesses.map(datum => ({
       ...datum,
@@ -35,6 +39,22 @@ function* loadDataWorker(action) {
     yield put(actions.loadDataSuccess('places', processedData));
   } catch (error) {
     yield put(actions.showError('places', error.message));
+  }
+}
+
+function* loadHoursData(action) {
+  console.log('loadHoursData was called');
+  console.log('  action:', action);
+}
+
+// in this loadData worker we'll split the logic based on the key
+function* loadDataWorker(action) {
+  const { key } = action.meta;
+
+  if (key === 'places') {
+    yield* loadPlacesData(action);
+  } else if (key === 'hours') {
+    yield* loadHoursData(action);
   }
 }
 
